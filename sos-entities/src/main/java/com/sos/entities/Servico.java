e01888e00e574040cd8224b9549fd8ba3d2dd92c
Diogo Peixoto <peixotoo@gmail.com>
2014-04-28 12:50:02 -0300

    Commitando a classe servico que foi esquecida.

---
+++ sos-entities/src/main/java/com/sos/entities/Servico.java
@@ -0,0 +1,105 @@
+package com.sos.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="servico")
+public class Servico implements Serializable {
+
+	private static final long serialVersionUID = 7108326588961989150L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id", updatable=false)
+	private Long id;
+	
+	@Column(name="valor", nullable=false)
+	private Double valor;
+	
+	@ManyToOne 
+    @JoinColumn(name="tipo_servico_id", nullable=false)
+	private TipoServico tipoServico;
+	
+	@Column(name="descricao", nullable=false, length=500)
+	private String descricao;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Servico [id=" + id + ", valor=" + valor + ", tipoServico="
+				+ tipoServico.getNome() + ", descricao=" + descricao + "]";
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public TipoServico getTipoServico() {
+		return tipoServico;
+	}
+
+	public void setTipoServico(TipoServico tipoServico) {
+		this.tipoServico = tipoServico;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+}
