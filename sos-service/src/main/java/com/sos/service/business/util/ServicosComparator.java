40440b2c8102ba51b3a63ecbf18ea8126b6c9fe2
Diogo Peixoto <peixotoo@gmail.com>
2014-07-13 14:48:43 -0300

    Ajustando busca dos servicos.

--- sos-service/src/main/java/com/sos/service/business/util/ServicosComparator.java
+++ sos-service/src/main/java/com/sos/service/business/util/ServicosComparator.java
@@ -8,16 +8,25 @@ public class ServicosComparator implements Comparator<Servico> {
 
 	@Override
 	public int compare(Servico servico1, Servico servico2) {
-		if(servico1.getPrestador().getNota() > servico2.getPrestador().getNota()){
-			return -1;
-		}else if(servico1.getPrestador().getNota() == servico2.getPrestador().getNota()){
-			if(servico1.getPrestador().getDistancia() <= servico2.getPrestador().getDistancia()){
+		
+		if(servico1.getPrestador().getNota() != null && servico2.getPrestador().getNota() != null){
+			if(servico1.getPrestador().getNota() > servico2.getPrestador().getNota()){
 				return -1;
+			}else if(servico1.getPrestador().getNota() == servico2.getPrestador().getNota()){
+				if(servico1.getPrestador().getDistancia() <= servico2.getPrestador().getDistancia()){
+					return -1;
+				}else{
+					return 1;
+				}
 			}else{
 				return 1;
 			}
 		}else{
-			return 1;
+			if(servico1.getPrestador() != null){
+				return -1;
+			}else{
+				return 1;
+			}
 		}
 	}
 }
